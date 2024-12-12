4cbfceed0572f0a27cad13f3f57d7111e6fa980f
Rodrigo Toledo <rodrigotoledo@tjro.jus.br>
2024-12-12 13:11:10 -0400

    testa novas formas de acesso ao BD, mas ainda apresenta conexão recusada

--- back/app/config.py
+++ back/app/config.py
@@ -4,5 +4,5 @@ import os
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').strip('"').strip("'")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- back/app/database.py
+++ back/app/database.py
@@ -1,12 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from .config import Config
+from app.config import Config
+import os
+from sqlalchemy import create_engine
+
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 engine = create_engine(Config.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db():
